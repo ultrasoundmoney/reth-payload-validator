5ea4c304875df924c4a2a970cc4d0e92ca051a03
ckoopmann <c.k.e.koopmann@gmail.com>
2023-09-30 10:53:17 +0800

    Adjust test script to avoid argument list too long on linux

--- test/shoot_single_payload.sh
+++ test/shoot_single_payload.sh
@@ -6,13 +6,11 @@ url="http://$1"
 # The path to the ndjson file
 file_path="$2"
 
-request=$(echo '{
+echo '{
         "jsonrpc":"2.0",
         "method":"flashbots_validateBuilderSubmissionV2",
         "params":[],
         "id":1
-    }' | jq --slurpfile payload ${file_path} '.params += $payload')
-
-echo "Sending POST request to ${url} with payload ${request}"
-
-curl -X POST -H "Content-Type: application/json" -d "${request}" ${url}
+    }' |\
+jq --slurpfile payload ${file_path} '.params += $payload' |\
+curl -X POST -H "Content-Type: application/json" -d @- "${url}"
