5dea8534902638532961541e6806d723f053b8fa
ckoopmann <c.k.e.koopmann@gmail.com>
2023-10-30 16:19:12 +0800

    Hardcode total difficulty to U256::MAX to avoid potential bypassing of the payment check

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -69,12 +69,11 @@ where
         let mut executor =
             EVMProcessor::new_with_db(chain_spec, StateProviderDatabase::new(state_provider));
 
-        let td = self.provider().header_td(&block.parent_hash).to_rpc_result()?.ok_or_else(
-            || internal_rpc_err(format!("Parent block {:?} not found", block.parent_hash)),
-        )?;
         let unsealed_block = block.clone().unseal();
+        // Note: Setting total difficulty to U256::MAX makes this incompatible with pre merge POW
+        // blocks
         executor
-            .execute_and_verify_receipt(&unsealed_block, td.add(block.difficulty), None)
+            .execute_and_verify_receipt(&unsealed_block, U256::MAX, None)
             .map_err(|e| internal_rpc_err(format!("Error executing transactions: {:}", e)))?;
 
         let output_state = executor.take_output_state();
