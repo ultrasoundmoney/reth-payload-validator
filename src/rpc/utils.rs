54fc29893313e32e8b70ebf89f3bb4f198815290
ckoopmann <c.k.e.koopmann@gmail.com>
2023-11-17 15:48:47 +0300

    Make calc_gas_limit implementation less confusing

--- src/rpc/utils.rs
+++ src/rpc/utils.rs
@@ -17,29 +17,26 @@ pub fn compare_values<T: std::cmp::PartialEq + std::fmt::Display>(
 }
 
 // TODO: Can we avoid hard coding this here and read it from reth config instead ?
+// Values as specified in: https://eips.ethereum.org/EIPS/eip-1559#specification
 const GAS_LIMIT_BOUND_DIVISOR: u64 = 1024;
 const MIN_GAS_LIMIT: u64 = 5000;
 
 // Compute the gas limit of the next block after parent. It aims
 // to keep the baseline gas close to the provided target, and increase it towards
 // the target if the baseline gas is lower.
-// Reference: https://github.com/flashbots/builder/blob/03ee71cf0a344397204f65ff6d3a917ee8e06724/core/utils/gas_limit.go#L8
+// Ported from: https://github.com/flashbots/builder/blob/03ee71cf0a344397204f65ff6d3a917ee8e06724/core/utils/gas_limit.go#L8
+// Reference implementation: https://eips.ethereum.org/EIPS/eip-1559#specification
 pub fn calc_gas_limit(parent_gas_limit: u64, desired_limit: u64) -> u64 {
+    // TODO: Understand why 1 is subtracted here
     let delta = parent_gas_limit / GAS_LIMIT_BOUND_DIVISOR - 1;
-    let mut limit = parent_gas_limit;
     let desired_limit = std::cmp::max(desired_limit, MIN_GAS_LIMIT);
-    if limit < desired_limit {
-        limit = parent_gas_limit + delta;
-        if limit > desired_limit {
-            limit = desired_limit;
-        }
-        return limit;
+    if parent_gas_limit < desired_limit {
+        let max_acceptable_limit = parent_gas_limit + delta;
+        std::cmp::min(max_acceptable_limit, desired_limit)
+    } else if parent_gas_limit > desired_limit {
+        let min_acceptable_limit = parent_gas_limit - delta;
+        std::cmp::max(min_acceptable_limit, desired_limit)
+    } else {
+        parent_gas_limit
     }
-    if limit > desired_limit {
-        limit = parent_gas_limit - delta;
-        if limit < desired_limit {
-            limit = desired_limit;
-        }
-    }
-    limit
 }
