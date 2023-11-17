bd17572a14cf1c2d68f14d2ace2d137dcc3adea3
ckoopmann <c.k.e.koopmann@gmail.com>
2023-11-17 13:17:43 +0300

    Add comment with link to reference implementation

--- src/rpc/utils.rs
+++ src/rpc/utils.rs
@@ -20,6 +20,10 @@ pub fn compare_values<T: std::cmp::PartialEq + std::fmt::Display>(
 const GAS_LIMIT_BOUND_DIVISOR: u64 = 1024;
 const MIN_GAS_LIMIT: u64 = 5000;
 
+// Compute the gas limit of the next block after parent. It aims
+// to keep the baseline gas close to the provided target, and increase it towards
+// the target if the baseline gas is lower.
+// Reference: https://github.com/flashbots/builder/blob/03ee71cf0a344397204f65ff6d3a917ee8e06724/core/utils/gas_limit.go#L8
 pub fn calc_gas_limit(parent_gas_limit: u64, desired_limit: u64) -> u64 {
     let delta = parent_gas_limit / GAS_LIMIT_BOUND_DIVISOR - 1;
     let mut limit = parent_gas_limit;
